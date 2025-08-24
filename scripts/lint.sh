89a15d91aa2220aa4a474dd2b35f1b3d1632eedf
AdrakPro <adam.makarewicz@proton.me>
2025-08-24 23:36:13 +0200

    Added: - Linter & black script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 set -e
 
+# Define paths
+PATHS="main.py src tests"
+
 echo "=============================="
 echo "Running isort (import sorting)"
 echo "=============================="
-python3.12 -m isort src tests
+python3.12 -m isort $PATHS
 
 echo "=============================="
 echo "Running Black (code formatting)"
 echo "=============================="
-python3.12 -m black src tests
+python3.12 -m black $PATHS
+
+echo "=============================="
+echo "Running autoflake (remove unused imports & variables)"
+echo "=============================="
+python3.12 -m pip install --quiet autoflake
+python3.12 -m autoflake --in-place --remove-unused-variables --remove-all-unused-imports -r $PATHS
 
 echo "=============================="
 echo "Running Flake8 (linting)"
 echo "=============================="
-python3.12 -m flake8 src tests
+python3.12 -m flake8 $PATHS
 
 echo "=============================="
 echo "All formatting and linting complete!"

Refactored: - BatchSize now is not fixed enum, its just list of ints - Changed fc1 units to work the same as batch_size - Translated Polish messages to English Added: - Crossover probability to config Fixed: - Now base value of calculating out_channels is taking in account width_scale

src/nn/data_loader.py
@@ -9,7 +9,7 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 from src.logger.experiment_logger import logger
-from src.model.chromosome import BatchSize, AugmentationIntensity
+from src.model.chromosome import AugmentationIntensity
 from src.utils.seed import seed_worker
 
 # Rule of thumb: cpu cores // 2
@@ -23,7 +23,7 @@ IMG_SIZE = 32
 
 
 def get_dataset_loaders(
-    batch_size: BatchSize,
+    batch_size: int,
     aug_intensity: AugmentationIntensity,
     is_gpu: bool,
     padding: int,
@@ -48,7 +48,7 @@ def get_dataset_loaders(
 
     train_loader = DataLoader(
         train_set,
-        batch_size=batch_size.value,
+        batch_size=batch_size,
         shuffle=True,
         num_workers=NUM_WORKERS,
         worker_init_fn=seed_worker,
@@ -57,7 +57,7 @@ def get_dataset_loaders(
 
     test_loader = DataLoader(
         test_set,
-        batch_size=batch_size.value,
+        batch_size=batch_size,
         shuffle=False,
         num_workers=NUM_WORKERS,
     )

src/nn/neural_network.py
@@ -44,7 +44,8 @@ class CNN(nn.Module):
         stride: int = config["fixed_parameters"]["stride"]
         padding: int = config["fixed_parameters"]["padding"]
 
-        base = 32
+        BASE_FILTERS = 32
+        base = int(BASE_FILTERS * chromosome.width_scale)
         out_channels = [base * (2**i) for i in range(conv_blocks)]
 
         layers = []
@@ -69,12 +70,7 @@ class CNN(nn.Module):
 
         # Dynamically calculate the flatten size for fc1 using a dummy forward pass
         with torch.no_grad():
-            dummy = torch.zeros(
-                1,
-                config["input_shape"][0],
-                config["input_shape"][1],
-                config["input_shape"][2],
-            )
+            dummy = torch.zeros(1, *config["input_shape"])
             out = self.conv_seq(dummy)
             fc1_in_features = out.view(1, -1).shape[1]
 
@@ -122,11 +118,13 @@ def get_optimizer_and_scheduler(
     Returns:
         Tuple of optimizer and scheduler (scheduler can be None).
     """
+    SGD_MOMENTUM = 0.9
+
     if chromosome.optimizer_schedule == OptimizerSchedule.SGD_STEP:
         optimizer = optim.SGD(
             model.parameters(),
             lr=chromosome.base_lr,
-            momentum=0.9,
+            momentum=SGD_MOMENTUM,
             weight_decay=chromosome.weight_decay,
         )
         scheduler = optim.lr_scheduler.StepLR(
@@ -136,7 +134,7 @@ def get_optimizer_and_scheduler(
         optimizer = optim.SGD(
             model.parameters(),
             lr=chromosome.base_lr,
-            momentum=0.9,
+            momentum=SGD_MOMENTUM,
             weight_decay=chromosome.weight_decay,
         )
         scheduler = optim.lr_scheduler.CosineAnnealingLR(